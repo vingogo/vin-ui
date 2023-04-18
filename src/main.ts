ff1f9ab56deb3e5d20adc833f65715a6e6a96323
Alvin <alvin.lm.liu@gmail.com>
2023-04-18 23:26:10 +0800

    feat: 添加多种风格颜色拓展

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { createSSRApp } from 'vue';
 import App from './App.vue';
 
+import '@vingogo/uni-ui/styles/extends/index.scss';
+
 export function createApp() {
   const app = createSSRApp(App);
 
